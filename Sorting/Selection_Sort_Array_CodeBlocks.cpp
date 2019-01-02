af2c2e00f3215157e1eb1a98ec1df0330074a19a
Sahdev Kansal <rockingkansal02@gmail.com>
2019-01-02 23:27:09 +0530

    Selection Sort added

---
+++ Sorting/Selection_Sort_Array_CodeBlocks.cpp
@@ -0,0 +1,44 @@
+//Program to sort an array using selection sort.
+#include<stdio.h>
+#include<cstdio>
+#include<cstdlib>
+#include<time.h>
+#define size 10 //Size of Array.
+void selection(int *,int);
+int main()
+{
+    int i,arr[size];
+    system("cls");
+    printf("\n Array before sorting is \n");
+    srand(time(NULL));
+    for(i = 0; i < size; i++)
+       {
+           arr[i] = rand()%100;
+           printf(" %d ",arr[i]);
+       }
+    printf("\n Array after sorting by is \n");
+    selection(arr,size);
+    for(i=0;i<size;i++)
+        printf(" %d ",arr[i]);
+    printf("\n");
+    system("pause");
+}
+
+void selection(int *arr,int n)
+{
+  int i,j,index,temp;
+  for(i=0;i<n;i++)
+  {
+      j=i;
+      index=j;
+      while(j<n)
+      {
+          if(arr[j]<arr[index])
+                index=j;
+          j++;
+      }
+    temp=arr[i];
+    arr[i]=arr[index];
+    arr[index]=temp;
+  }
+}
---
+++ Sorting/Selection_Sort_Linked_List_Codeblocks.cpp
@@ -0,0 +1,76 @@
+//Program to sort a linked list using selection sort.
+#include<stdio.h>
+#include<time.h>
+#include<cstdlib>
+#define size 10 //Size of linked list.
+struct node
+{
+    int info;
+    struct node *next;
+}*start=NULL;
+typedef struct node * Node;
+int main()
+{
+    int i;
+    Node ptr;
+    void selectionsort(Node);
+    srand(time(NULL));
+    for(i=0;i<size;i++)
+    {
+        Node ptr,temp;
+        ptr=(Node)malloc(sizeof(node));
+        ptr->info=rand()%100; //Random linked list of given size is created.
+        ptr->next=NULL;
+        if(start==NULL)
+        {
+            start=ptr;
+        }
+        else
+        {  temp=start;
+           while(temp->next!=NULL)
+              temp=temp->next;
+           temp->next=ptr;
+        }
+    }
+    printf(" Linked List Before Sorting Is -\n");
+    ptr=start;
+    while(ptr!=NULL)
+    {
+        printf(" %d ",ptr->info);
+        ptr=ptr->next;
+    }
+    printf("\n Linked List After Sorting Is -\n");
+    selectionsort(start);
+    ptr=start;
+    while(ptr!=NULL)
+    {
+        printf(" %d ",ptr->info);
+        ptr=ptr->next;
+    }
+    return 0;
+}
+
+void selectionsort(Node start)
+{
+    Node ptr=start,temp,address;
+    int var;
+    while(ptr->next!=NULL)
+    {
+        temp=ptr;
+        address=temp;
+        while(temp!=NULL)
+        {
+            if((address->info)>(temp->info))
+                address=temp;
+            temp=temp->next;
+        }
+
+        var=address->info;
+        address->info=ptr->info;
+        ptr->info=var;
+        ptr=ptr->next;
+    }
+
+}
+
+
